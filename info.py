4f241b8d0d3a946ad182fe8807d0ea22e63410de
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2025-06-23 15:51:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 
 # ForceSub Channel & Log Channels
 AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', '-1002560313401'))
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002714383718'))
+AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002560313401'))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002289263173'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002737733323')) 
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002737733323'))
