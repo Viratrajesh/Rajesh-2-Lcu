a6ec5e7a9681516651f24dfa990751bb58a0e32a
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2025-06-24 10:05:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -111,7 +111,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://t.me/MTGxStreamBot")
+URL = environ.get("FQDN", "https://t.me/MTGxStreamBot.koyeb.app")
 
 # Commands
 admin_cmds = [
