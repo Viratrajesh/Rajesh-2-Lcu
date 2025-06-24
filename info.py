3c7375d667948b770851e5383694c9e68fce9422
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2025-06-24 10:21:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -93,7 +93,7 @@ MAX_BTN = int(environ.get('MAX_BTN', '8'))
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 DELETE_TIME = int(environ.get('DELETE_TIME', 300))
 IMDB = is_enabled('IMDB', False)
-FILE_CAPTION = "📁 <b>{file_name}</b>\n📦 \n\n⚡ Powered by @Askmovies4"
+FILE_CAPTION = "📁 <b>{file_name}</b>\n \n\n⚡ Powered by @Askmovies4"
 IMDB_TEMPLATE = environ.get('IMDB_TEMPLATE', f'{script.IMDB_TEMPLATE_TXT}')
 LONG_IMDB_DESCRIPTION = is_enabled('LONG_IMDB_DESCRIPTION', False)
 PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
@@ -111,7 +111,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://www.askmovies.workers.dev")
+URL = environ.get("FQDN", "https://www.askmovies.workers.dev.koyeb.app")
 
 # Commands
 admin_cmds = [
