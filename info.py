e66b3dbebce4e764d881ae0053597ad150cab01b
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2025-06-25 23:17:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -111,7 +111,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "https://www.askmovies.workers.dev")
+URL = environ.get("FQDN", "https://theoretical-ocelot-createtelegrambot-10eace31.koyeb.app/")
 
 # Commands
 admin_cmds = [
