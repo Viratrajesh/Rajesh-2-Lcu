e0064ea6d9c87829c8a5d2ab6e4536c3c1996913
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2025-07-09 08:49:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -93,7 +93,7 @@ MAX_BTN = int(environ.get('MAX_BTN', '8'))
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 DELETE_TIME = int(environ.get('DELETE_TIME', 300))
 IMDB = is_enabled('IMDB', False)
-FILE_CAPTION = """<b>🎬 <a href="https://t.me/Askmovieslink1">{file_name}</a></b>                                                           📤 Uploaded by: @AskMovies4"""
+FILE_CAPTION = """<b>🎬 <a href="https://t.me/Askmovieslink1">{file_name}</a></b>                                                           /n📤 Uploaded by: @AskMovies4"""
 IMDB_TEMPLATE = environ.get('IMDB_TEMPLATE', f'{script.IMDB_TEMPLATE_TXT}')
 LONG_IMDB_DESCRIPTION = is_enabled('LONG_IMDB_DESCRIPTION', False)
 PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
